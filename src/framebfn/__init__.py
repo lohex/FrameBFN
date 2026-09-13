ef7aa6b3bbafc672e9cd574a07feb8e7c7c2a1d1
Lorenz Hexemer <67096411+lohex@users.noreply.github.com>
2026-09-13 15:31:01 +0200

    Add src/framebfn/__init__.py

---
+++ src/framebfn/__init__.py
@@ -0,0 +1,13 @@
+"""Public FrameBFN peptide-data utilities."""
+
+from .data import load_dyndb_trajectory
+from .geometry import extract_backbone_angles
+from .plotting import per_residue_ramachandra, per_residue_ramachandran
+
+__all__ = [
+    "extract_backbone_angles",
+    "load_dyndb_trajectory",
+    "per_residue_ramachandra",
+    "per_residue_ramachandran",
+]
+
